b9ffe36f8b7eb4fcd70a6e029f553046b1e3c3e1
Bandana Irmal A <47946762+biairmal@users.noreply.github.com>
2021-12-10 08:45:40 +0700

    web socket comments

--- src/api/v1/utils/WebSocket.js
+++ src/api/v1/utils/WebSocket.js
@@ -6,7 +6,7 @@ class WebSocket {
     
     // create online users array
     for (let [id, socket] of io.of('/').sockets) {
-      this.users.push({
+      users.push({
         userID: id,
         username: socket.username,
       })
@@ -24,47 +24,47 @@ class WebSocket {
       console.log('user connected', socket)
     })
 
-    // send private message to specified user
-    socket.on('private message', ({ content, to }) => {
-      socket.to(to).emit('private message', {
-        content,
-        from: socket.id,
-      })
-    })
+    // // send private message to specified user
+    // socket.on('private message', ({ content, to }) => {
+    //   socket.to(to).emit('private message', {
+    //     content,
+    //     from: socket.id,
+    //   })
+    // })
 
-    // send session details to user
-    socket.emit('session', {
-      sessionID: socket.sessionID,
-      userID: socket.userID,
-    })
+    // // send session details to user
+    // socket.emit('session', {
+    //   sessionID: socket.sessionID,
+    //   userID: socket.userID,
+    // })
 
-    // make the socket instance join the associated room
-    socket.join(socket.userID)
+    // // make the socket instance join the associated room
+    // socket.join(socket.userID)
 
-    // update the forwarding handler
-    socket.on('private message', ({ content, to }) => {
-      socket.to(to).to(socket.userID).emit('private message', {
-        content: content,
-        from: socket.userID,
-        to: to,
-      })
-    })
+    // // update the forwarding handler
+    // socket.on('private message', ({ content, to }) => {
+    //   socket.to(to).to(socket.userID).emit('private message', {
+    //     content: content,
+    //     from: socket.userID,
+    //     to: to,
+    //   })
+    // })
 
-    socket.on('disconnect', async () => {
-      const matchingSockets = await global.io.in(socket.userID).allSockets()
-      const isDisconnected = matchingSockets.size = 0
-      if(isDisconnected) {
-        // notify other users
-        socket.broadcast.emit('users disconnected', socket.userID)
+    // socket.on('disconnect', async () => {
+    //   const matchingSockets = await global.io.in(socket.userID).allSockets()
+    //   const isDisconnected = matchingSockets.size = 0
+    //   if(isDisconnected) {
+    //     // notify other users
+    //     socket.broadcast.emit('users disconnected', socket.userID)
 
-        // update the connection status of the session
-        sessionStore.saveSession(socket.sessionID, {
-          userID: socket.userID,
-          username: socket.username,
-          connected: false,
-        })
-      }
-    })
+    //     // update the connection status of the session
+    //     sessionStore.saveSession(socket.sessionID, {
+    //       userID: socket.userID,
+    //       username: socket.username,
+    //       connected: false,
+    //     })
+    //   }
+    // })
   }
 }
 
